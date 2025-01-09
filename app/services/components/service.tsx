94ca258b02c87c84e484b489f4a94ec277be551e
unknown <luism@usmarttech.com>
2025-01-09 09:33:34 -0500

    Bug fixes

--- app/services/components/service.tsx
+++ app/services/components/service.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client"
 
 import { motion } from "framer-motion";
 import Carousel from "./carousel";
@@ -9,6 +9,7 @@ import ConsultingList from "./consultingList";
 import DevelopmentList from "./developmentList";
 import Workflow from "./workflow";
 import Header from "./header";
+import Image from "next/image";
 
 type Section = "section1" | "section2" | "section3" | null;
 
@@ -32,7 +33,7 @@ export default function Service() {
     const handleGoBack = () => {
         setShowMoreSection(null);
         setTimeout(() => {
-            window.scrollTo({ top: 600, behavior: 'smooth' });
+            window.scrollTo({ top: 600, behavior: "smooth" });
         }, 300); // Asegura que la animación se complete antes de desplazar
     };
 
@@ -41,7 +42,7 @@ export default function Service() {
             setTimeout(() => {
                 if (sectionRefs[showMoreSection].current) {
                     sectionRefs[showMoreSection].current.scrollIntoView({
-                        behavior: 'smooth'
+                        behavior: "smooth"
                     });
                 }
             }, 500); // Asegura que la animación se complete antes de desplazar 
@@ -53,9 +54,9 @@ export default function Service() {
             <Header />
             <div className="grid items-center grid-cols-3 p-10 flex w-full h-full bg-sky-950">
                 <div id="automation" className="relative w-[600px] h-[700px] rounded-[10px] overflow-hidden 
-          before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
-          before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
-          before:animate-spin-slow group" data-aos="fade-up-right">
+                    before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
+                    before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
+                    before:animate-spin-slow group" data-aos="fade-up-right">
                     <div className="absolute flex flex-col gap-10 items-center top-[5px] bottom-[5px] left-[5px] right-[5px] rounded-[10px] bg-white bg-[url(../public/rb_23189.png)] bg-cover bg-no-repeat">
                         <h3 className="text-4xl pt-20 select-none text-left">Automation</h3>
                         <p className="text-2xl select-none text-justify p-10 backdrop-blur-2xl">Adjust and adapt AI agent features to meet your company's specific needs and preferences</p>
@@ -68,15 +69,15 @@ export default function Service() {
                             whileHover={{ scale: 1.3 }}
                             onClick={() => toggleShowMore('section1')}
                         >
-                            <img src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
+                            <Image src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
                             Show more
                         </motion.button>
                     </div>
                 </div>
                 <div id="consulting" className="relative w-[600px] h-[700px] rounded-[10px] overflow-hidden 
-          before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
-          before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
-          before:animate-spin-slow group" data-aos="fade-up">
+                    before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
+                    before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
+                    before:animate-spin-slow group" data-aos="fade-up">
                     <div className="absolute flex flex-col items-center gap-10 top-[5px] bottom-[5px] left-[5px] right-[5px] rounded-[10px] bg-white bg-[url(../public/673.png)] bg-cover bg-no-repeat">
                         <h3 className="text-4xl pt-20 select-none">Consulting</h3>
                         <p className="text-2xl text-justify p-10 select-none backdrop-blur-2xl">Software and technology to execute repetitive and routine tasks that save time, reduce errors and increase operational efficiency in various areas, from business management to software development</p>
@@ -88,15 +89,15 @@ export default function Service() {
                             whileHover={{ scale: 1.3 }}
                             onClick={() => toggleShowMore('section2')}
                         >
-                            <img src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
+                            <Image src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
                             Show more
                         </motion.button>
                     </div>
                 </div>
                 <div id="development" className="relative w-[600px] h-[700px] rounded-[10px] overflow-hidden 
-          before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
-          before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
-          before:animate-spin-slow group" data-aos="fade-up-left">
+                    before:absolute before:top-[-50%] before:bottom-[-50%] before:right-[-50%] before:left-[-50%] 
+                    before:bg-[conic-gradient(transparent,#594A28,#00a6ff)]
+                    before:animate-spin-slow group" data-aos="fade-up-left">
                     <div className="absolute flex flex-col items-center gap-5 top-[5px] bottom-[5px] left-[5px] right-[5px] rounded-[10px] bg-white bg-[url(../public/rb_5102.png)] bg-cover bg-no-repeat">
                         <h3 className="text-4xl pt-20 pb-5 select-none text-center select-none">Development</h3>
                         <p className="text-2xl pl-10 pr-10 text-justify text-center select-none backdrop-blur-2xl">Maximize your team's efficiency and productivity with our integration solution that effortlessly connects with the most popular tools on the market. Our platform makes your workflow easier by uniting your favorite tools in one place</p>
@@ -111,17 +112,17 @@ export default function Service() {
                             whileHover={{ scale: 1.3 }}
                             onClick={() => toggleShowMore('section3')}
                         >
-                            <img src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
+                            <Image src="/arrow_down.svg" alt="arrow_down" width={30} height={40} />
                             Show more
                         </motion.button>
                     </div>
                 </div>
             </div>
-            {showMoreSection === 'section1' && (
+            {showMoreSection === "section1" && (
                 <motion.div ref={sectionRefs.section1}
                     className="flex flex-col gap-5 text-justify p-20 border-2 border-[#594A28] bg-white"
                     initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: 'auto', opacity: 1 }}
+                    animate={{ height: "auto", opacity: 1 }}
                     transition={{ duration: 0.5 }} >
                     <h2 className="text-4xl select-none">The #1 AI and Automation Agency</h2>
                     <p className="text-3xl select-none">We combine artificial intelligence with automation to create incredibly efficient processes that connect your apps and automate actions between them, which replaces manual work for massive gains in your team’s bandwidth, quality, speed and customer experience. Fully custom and done-for-you</p>
@@ -131,25 +132,25 @@ export default function Service() {
                         </div>
                         <div className="grid grid-cols-4 z-2 gap-10 p-10 absolute lg:top-40 flex gap-10">
                             <div className="flex flex-col items-center gap-3">
-                                <img src="/process.svg" alt="process" width={100} height={100} className="bg-white" />
+                                <Image src="/process.svg" alt="process" width={100} height={100} className="bg-white" />
                                 <span className="pl-4 text-lg bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-700">Step 1</span>
                                 <h4 className="text-lg font-bold select-none">We map out your processes</h4>
                                 <p className="select-none text-center w-[300px]">We’ll create a visual map of all your systems, manual tasks and apps.</p>
                             </div>
                             <div className="flex flex-col items-center gap-3">
-                                <img src="/find.svg" alt="process" width={100} height={100} className="bg-white" />
+                                <Image src="/find.svg" alt="process" width={100} height={100} className="bg-white" />
                                 <span className="pl-4 text-lg bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-700">Step 2</span>
                                 <h4 className="text-lg font-bold select-none">We find areas to add AI and automate</h4>
                                 <p className="select-none text-center w-[300px]">We audit your workflows to pinpoint opportunities with the highest ROI.</p>
                             </div>
                             <div className="flex flex-col items-center gap-3">
-                                <img src="/test.svg" alt="process" width={100} height={100} className="bg-white" />
+                                <Image src="/test.svg" alt="process" width={100} height={100} className="bg-white" />
                                 <span className="pl-4 text-lg bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-700">Step 3</span>
                                 <h4 className="text-lg font-bold select-none">We build and test</h4>
                                 <p className="select-none text-center w-[300px]">We use a mix of custom code, AI tools, Zapier, Make.com and your tech stack.</p>
                             </div>
                             <div className="flex flex-col items-center gap-3">
-                                <img src="/manage.svg" alt="process" width={100} height={100} className="bg-white" />
+                                <Image src="/manage.svg" alt="process" width={100} height={100} className="bg-white" />
                                 <span className="pl-4 text-lg bg-clip-text text-transparent bg-gradient-to-r from-cyan-500 to-blue-700">Step 4</span>
                                 <h4 className="text-lg font-bold select-none">We manage and iterate</h4>
                                 <p className="select-none text-center w-[300px]">Every client we work with grows, so there’s always new things to automate.</p>
@@ -163,12 +164,12 @@ export default function Service() {
                             whileHover={{ scale: 1.3 }}
                             style={{ transformOrigin: 'center center' }}
                         >
-                            <img src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
+                            <Image src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
                             Go back
                         </motion.button>
                     </div>
                 </motion.div>)}
-            {showMoreSection === 'section2' && (
+            {showMoreSection === "section2" && (
                 <motion.div ref={sectionRefs.section2}
                     className="flex flex-col gap-5 text-justify p-20 border-2 border-[#594A28] bg-white"
                     initial={{ height: 0, opacity: 0 }}
@@ -177,37 +178,37 @@ export default function Service() {
                     <h2 className="text-4xl">Consulting</h2>
                     <p className="text-3xl">We combine artificial intelligence with automation to create incredibly efficient processes that connect your apps and automate actions between them, which replaces manual work for massive gains in your team’s bandwidth, quality, speed and customer experience. Fully custom and done-for-you</p>
                     {/*En esta parte
-          Se puede agregar calquier contenido
-          Preferible utilizar otros componentes*/}
+                        Se puede agregar calquier contenido
+                        Preferible utilizar otros componentes*/}
                     <div className="flex jusify-center">
                         <motion.button className="relative flex gap-2 items-center mt-4 text-[#594A28] px-4 py-2"
                             onClick={handleGoBack}
                             whileHover={{ scale: 1.3 }}
-                            style={{ transformOrigin: 'center center' }}
+                            style={{ transformOrigin: "center center" }}
                         >
-                            <img src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
+                            <Image src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
                             Go back
                         </motion.button>
                     </div>
                 </motion.div>)}
-            {showMoreSection === 'section3' && (
+            {showMoreSection === "section3" && (
                 <motion.div ref={sectionRefs.section1}
                     className="flex flex-col gap-5 text-justify p-20 border-2 border-[#594A28] bg-white"
                     initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: 'auto', opacity: 1 }}
+                    animate={{ height: "auto", opacity: 1 }}
                     transition={{ duration: 0.5 }} >
                     <h2 className="text-4xl">The #1 Development Agency</h2>
                     <p className="text-3xl">We combine artificial intelligence with automation to create incredibly efficient processes that connect your apps and automate actions between them, which replaces manual work for massive gains in your team’s bandwidth, quality, speed and customer experience. Fully custom and done-for-you</p>
                     {/*En esta parte
-          Se puede agregar calquier contenido
-          Preferible utilizar otros componentes*/}
+                        Se puede agregar calquier contenido
+                        Preferible utilizar otros componentes*/}
                     <div className="flex jusify-center">
                         <motion.button className="relative flex gap-2 items-center mt-4 text-[#594A28] px-4 py-2"
                             onClick={handleGoBack}
                             whileHover={{ scale: 1.3 }}
-                            style={{ transformOrigin: 'center center' }}
+                            style={{ transformOrigin: "center center" }}
                         >
-                            <img src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
+                            <Image src="/arrow_up.svg" alt="arrow_up" width={30} height={40} />
                             Go back
                         </motion.button>
                     </div>
