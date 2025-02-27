43deaffd6e63f0cd2e71fb7814358ae529535d88
unknown <luism@usmarttech.com>
2025-02-27 14:03:41 -0500

    Adding automation's sections

--- app/services/components/ai-agents.tsx
+++ app/services/components/ai-agents.tsx
@@ -1,9 +1,50 @@
+import Image from "next/image";
 
 export default function AiAgents() {
     return (
-        <section className="lg:flex lg:items-center lg:pl-5 lg:pr-5 lg:pt-20 lg:pb-20 lg:gap-16 lg:w-full flex gap-10 pt-16 w-screen" data-aos="fade-up">
-            <h3 className="text-2xl text-[#010817]">AI Agents</h3>
+        <section className="flex flex-col items-center w-full gap-10 pt-10" data-aos="fade-up">
+                <h3 className="font-bold text-3xl text-[#03a7b3]">AI Agents</h3>
+                <p className="w-[900px] text-center text-lg">Our AI agents are designed to transform the way you interact with technology.
+                    We offer <span className="bg-gradient-to-r from-[#03a7b3] to-[#345d87] bg-clip-text text-transparent font-bold">Customized AI</span> solutions that adapt to the specific needs of your business,
+                    improving efficiency and reducing operational costs.</p>
 
+                <div className="grid grid-cols-3 gap-20 p-20">
+                    <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                        <Image 
+                            alt="va"
+                            src="/va.svg"
+                            width={100}
+                            height={100}
+                        />
+                        <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Virtual Assistance:</h4>
+                        <p className="text-center">Improve customer service with virtual assistants that can handle common queries and free up time for your human team.</p> 
+                    </div>
+                    <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                        <Image 
+                            alt="integration"
+                            src="/integration.svg"
+                            width={75}
+                            height={75}
+                            className="m-3"
+                        />
+                        <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Seamless Integration:</h4>
+                        <p className="text-center">Our AI agents easily integrate with your existing systems to optimize and automate internal processes.</p>
+                    </div>
+                    <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                        <Image 
+                            alt="analytics"
+                            src="/analytics2.svg"
+                            width={75}
+                            height={75}
+                            className="m-3"
+                        />
+                        <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Intelligent Analytics:</h4>
+                        <p className="text-center">Use the power of our AI agents to analyze data in real time and deliver actionable insights that drive informed business decisions.</p>
+                    </div>
+                </div>
+                <div>
+                    Insert demo here!
+                </div>
         </section>
     );
 }
--- app/services/components/workflows.tsx
+++ app/services/components/workflows.tsx
@@ -1,9 +1,47 @@
-
+import Image from "next/image";
 
 export default function Workflows() {
     return (
-        <section className="lg:flex lg:items-center lg:pl-5 lg:pr-5 lg:pt-20 lg:pb-20 lg:gap-16 lg:w-full flex gap-10 pt-16 w-screen" data-aos="fade-up">
-            <h3 className="text-2xl text-[#03a7b3]">Workflows</h3>
+        <section className="flex flex-col items-center w-full gap-10 pt-10" data-aos="fade-up">
+            <h3 className="font-bold text-3xl text-[#03a7b3]">Workflows</h3>
+            <p className="w-[900px] text-center text-lg">Optimize and automate your repetitive tasks with our advanced workflow solutions, designed to improve your team&apos;s
+                <span className="bg-gradient-to-r from-[#03a7b3] to-[#345d87] bg-clip-text text-transparent font-bold">Productivity</span> and <span className="bg-gradient-to-r from-[#03a7b3] to-[#345d87] bg-clip-text text-transparent font-bold">Efficiency</span>.</p>
+            <div className="grid grid-cols-3 gap-20 p-20">
+                <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                    <Image 
+                        alt="tasks"
+                        src="/tasks.svg"
+                        width={100}
+                        height={100}
+                        className="m-3"
+                    />
+                    <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Automation of Repetitive Tasks:</h4>
+                    <p className="text-center">Eliminate manual and repetitive tasks through automation, allowing your team to focus on higher-value activities.</p>
+                </div>
+                <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                    <Image 
+                        alt="workflow"
+                        src="/workflows.svg"
+                        width={100}
+                        height={100}
+                        className="m-3"
+                    />
+                    <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Customized Workflows:</h4>
+                    <p className="text-center">We create workflows adapted to your specific processes, ensuring a fluid and efficient implementation.</p>
+                </div>
+                <div className="flex flex-col items-center gap-2 w-[400px] border-2 border-[#3c4442] rounded-lg p-5 hover:transform hover:-translate-y-2 hover:shadow-xl transition duration-300">
+                    <Image 
+                        alt="improve"
+                        src="/improve.svg"
+                        width={100}
+                        height={100}
+                        className="m-3"
+                    />
+                    <h4 className="font-bold bg-gradient-to-b from-[#03a7b3] to-[#010817] bg-clip-text text-transparent text-lg">Monitoring and Continuous Improvement:</h4>
+                    <p className="text-center">Our workflows include monitoring and analysis capabilities to identify opportunities for improvement and ensure optimal performance.</p>
+                </div>
+            </div>
+            Insert demo here!
         </section>
     );
 }
